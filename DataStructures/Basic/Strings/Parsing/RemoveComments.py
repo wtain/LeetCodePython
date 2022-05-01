fcedc97514f3ece836e0e6c64bf576ecd91edf29
Ramiz <ramiz.zeynalov@gmail.com>
2022-05-01 17:41:05 +0200

    Sync

---
+++ DataStructures/Basic/Strings/Parsing/RemoveComments.py
@@ -0,0 +1,168 @@
+"""
+https://leetcode.com/problems/remove-comments/
+
+Given a C++ program, remove comments from it. The program source is an array of strings source where source[i] is the ith line of the source code. This represents the result of splitting the original source code string by the newline character '\n'.
+
+In C++, there are two types of comments, line comments, and block comments.
+
+The string "//" denotes a line comment, which represents that it and the rest of the characters to the right of it in the same line should be ignored.
+The string "/*" denotes a block comment, which represents that all characters until the next (non-overlapping) occurrence of "*/" should be ignored. (Here, occurrences happen in reading order: line by line from left to right.) To be clear, the string "/*/" does not yet end the block comment, as the ending would be overlapping the beginning.
+The first effective comment takes precedence over others.
+
+For example, if the string "//" occurs in a block comment, it is ignored.
+Similarly, if the string "/*" occurs in a line or block comment, it is also ignored.
+If a certain line of code is empty after removing comments, you must not output that line: each string in the answer list will be non-empty.
+
+There will be no control characters, single quote, or double quote characters.
+
+For example, source = "string s = "/* Not a comment. */";" will not be a test case.
+Also, nothing else such as defines or macros will interfere with the comments.
+
+It is guaranteed that every open block comment will eventually be closed, so "/*" outside of a line or block comment always starts a new comment.
+
+Finally, implicit newline characters can be deleted by block comments. Please see the examples below for details.
+
+After removing the comments from the source code, return the source code in the same format.
+
+
+
+Example 1:
+
+Input: source = ["/*Test program */", "int main()", "{ ", "  // variable declaration ", "int a, b, c;", "/* This is a test", "   multiline  ", "   comment for ", "   testing */", "a = b + c;", "}"]
+Output: ["int main()","{ ","  ","int a, b, c;","a = b + c;","}"]
+Explanation: The line by line code is visualized as below:
+/*Test program */
+int main()
+{
+  // variable declaration
+int a, b, c;
+/* This is a test
+   multiline
+   comment for
+   testing */
+a = b + c;
+}
+The string /* denotes a block comment, including line 1 and lines 6-9. The string // denotes line 4 as comments.
+The line by line output code is visualized as below:
+int main()
+{
+
+int a, b, c;
+a = b + c;
+}
+Example 2:
+
+Input: source = ["a/*comment", "line", "more_comment*/b"]
+Output: ["ab"]
+Explanation: The original source string is "a/*comment\nline\nmore_comment*/b", where we have bolded the newline characters.  After deletion, the implicit newline characters are deleted, leaving the string "ab", which when delimited by newline characters becomes ["ab"].
+
+
+Constraints:
+
+1 <= source.length <= 100
+0 <= source[i].length <= 80
+source[i] consists of printable ASCII characters.
+Every open block comment is eventually closed.
+There are no single-quote or double-quote in the input.
+"""
+from typing import List
+
+from Common.ObjectTestingUtils import run_functional_tests
+
+
+# class Solution:
+#     def removeComments(self, source: List[str]) -> List[str]:
+#         result = []
+#         is_comment = False
+#         for line in source:
+#             if is_comment:
+#                 pos = line.find('*/')
+#                 if pos == -1:
+#                     continue
+#                 is_comment = False
+#                 line = line[pos+2:]
+#             in_string = False
+#             prev_char = None
+#             for i, c in enumerate(line):
+#                 if not in_string:
+#                     if c == '"':
+#                         in_string = True
+#                     elif c == '/':
+#                         if prev_char == '/':
+#                             line = line[:i-1]
+#                             break
+#                     elif c == '*':
+#                         if prev_char == '/':
+#                             line = line[:i - 1]
+#                             is_comment = True
+#                             break
+#                 else:
+#                     if c == '\\':
+#                         pass
+#                     elif c == '"' and prev_char != '\\':
+#                         in_string = True
+#                 prev_char = c
+#             result.append(line)
+#
+#         return result
+
+
+# Runtime: 30 ms, faster than 91.41% of Python3 online submissions for Remove Comments.
+# Memory Usage: 13.8 MB, less than 88.96% of Python3 online submissions for Remove Comments.
+# https://leetcode.com/problems/remove-comments/discuss/1987846/Python-Iteration
+class Solution:
+    def removeComments(self, source: List[str]) -> List[str]:
+        result, buffer, is_comment = [], "", False
+        for line in source:
+            index = 0
+            while index < len(line):
+                if line[index] == '/' and index + 1 < len(line) and line[index+1] == '/' and not is_comment:
+                    index = len(line)
+                elif line[index] == '/' and index + 1 < len(line) and line[index+1] == '*' and not is_comment:
+                    is_comment = True
+                    index += 2
+                elif line[index] == '*' and index + 1 < len(line) and line[index + 1] == '/' and is_comment:
+                    is_comment = False
+                    index += 2
+                elif is_comment:
+                    index += 1
+                else:
+                    buffer += line[index]
+                    index += 1
+            if not is_comment and buffer:
+                result.append(buffer)
+                buffer = ""
+        return result
+
+
+tests = [
+    [
+        [
+            "/*Test program */",
+            "int main()",
+            "{ ",
+            "  // variable declaration ",
+            "int a, b, c;",
+            "/* This is a test",
+            "   multiline  ",
+            "   comment for ",
+            "   testing */",
+            "a = b + c;",
+            "}"
+        ],
+        [
+            "int main()",
+            "{ ",
+            "  ",
+            "int a, b, c;",
+            "a = b + c;",
+            "}"
+        ]
+    ],
+    [
+        ["a/*comment", "line", "more_comment*/b"],
+        ["ab"]
+    ]
+]
+
+run_functional_tests(Solution().removeComments, tests)
