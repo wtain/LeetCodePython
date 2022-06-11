dd75fbb70f6ca5315ab200fe3f0a0402fb5204d1
Ramiz <ramiz.zeynalov@gmail.com>
2022-06-11 02:22:47 +0200

    Daily

--- Algorithms/Basic/SlidingWindow/MinimumOperationsToReduceXToZero.py
+++ Algorithms/Basic/SlidingWindow/MinimumOperationsToReduceXToZero.py
@@ -88,49 +88,114 @@ from typing import List
 from Common.ObjectTestingUtils import run_functional_tests
 
 
+# WRONG
+# class Solution:
+#     def minOperations(self, nums: List[int], x: int) -> int:
+#         n = len(nums)
+#         s = sum(nums)
+#         t = s - x
+#         if t == 0:
+#             return n
+#         current = 0
+#         i1 = 0
+#         i2 = 0
+#         longest = 0
+#         while i1 < n and i2 < n:
+#             if current < t:
+#                 current += nums[i2]
+#                 i2 += 1
+#             elif current > t:
+#                 current -= nums[i1]
+#                 i1 += 1
+#             else:
+#                 longest = max(longest, i2-i1)
+#                 current -= nums[i1]
+#                 i1 += 1
+#         if current == t:
+#             longest = max(longest, i2-i1)
+#         if longest == 0:
+#             return -1
+#         return n - longest
+
+
+# WRONG
+# class Solution:
+#     def minOperations(self, nums: List[int], x: int) -> int:
+#         n, s = len(nums), sum(nums)
+#         t = s - x
+#         if t == 0:
+#             return n
+#         current, i1, i2, longest = 0, 0, 0, 0
+#         while i1 < n and i2 < n:
+#             if current < t:
+#                 current += nums[i2]
+#                 i2 += 1
+#             elif current > t:
+#                 current -= nums[i1]
+#                 i1 += 1
+#             else:
+#                 longest = max(longest, i2-i1)
+#                 current -= nums[i1]
+#                 i1 += 1
+#         if current == t:
+#             longest = max(longest, i2-i1)
+#         if longest == 0:
+#             return -1
+#         return n - longest
+
+
+# Runtime: 1251 ms, faster than 85.38% of Python3 online submissions for Minimum Operations to Reduce X to Zero.
+# Memory Usage: 27.9 MB, less than 82.08% of Python3 online submissions for Minimum Operations to Reduce X to Zero.
+# https://leetcode.com/problems/minimum-operations-to-reduce-x-to-zero/discuss/2045072/Simple-C%2B%2B-code-ororO(N)-time-oror-O(1)-space
 class Solution:
     def minOperations(self, nums: List[int], x: int) -> int:
         n = len(nums)
-        s = sum(nums)
-        t = s - x
+        l, r, t = -1, n-1, x
+        BIG_VAL = 10 ** 12
+        store = BIG_VAL
+        while t > 0 and l < n-1:
+            l += 1
+            t -= nums[l]
         if t == 0:
-            return n
-        current = 0
-        i1 = 0
-        i2 = 0
-        longest = 0
-        while i1 < n and i2 < n:
-            if current < t:
-                current += nums[i2]
-                i2 += 1
-            elif current > t:
-                current -= nums[i1]
-                i1 += 1
+            store = l+1
+        while l < r:
+            if t < 0 and l > -1:
+                t += nums[l]
+                l -= 1
+            elif t > 0 and r >= -1:
+                t -= nums[r]
+                r -= 1
+            elif t == 0:
+                calc = n - r + l
+                store = min(store, calc)
+                if l > -1:
+                    t += nums[l]
+                    l -= 1
+                if r > -1:
+                    t -= nums[r]
+                    r -= 1
             else:
-                longest = max(longest, i2-i1)
-                current -= nums[i1]
-                i1 += 1
-        if current == t:
-            longest = max(longest, i2-i1)
-        if longest == 0:
+                break
+        if store == BIG_VAL:
             return -1
-        return n - longest
-
+        return store
 
 
 tests = [
 
-    ([5,2,3,1,1], 5, 1),
+    [[5,1,4,2,3], 6, 2],
 
-    ([8828,9581,49,9818,9974,9869,9991,10000,10000,10000,9999,9993,9904,8819,1231,6309], 134365, 16),
+    [[5,2,3,1,1], 5, 1],
 
-    ([5207,5594,477,6938,8010,7606,2356,6349,3970,751,5997,6114,9903,3859,6900,7722,2378,1996,8902,228,4461,90,7321,7893,4879,9987,1146,8177,1073,7254,5088,402,4266,6443,3084,1403,5357,2565,3470,3639,9468,8932,3119,5839,8008,2712,2735,825,4236,3703,2711,530,9630,1521,2174,5027,4833,3483,445,8300,3194,8784,279,3097,1491,9864,4992,6164,2043,5364,9192,9649,9944,7230,7224,585,3722,5628,4833,8379,3967,5649,2554,5828,4331,3547,7847,5433,3394,4968,9983,3540,9224,6216,9665,8070,31,3555,4198,2626,9553,9724,4503,1951,9980,3975,6025,8928,2952,911,3674,6620,3745,6548,4985,5206,5777,1908,6029,2322,2626,2188,5639], 565610, 113),
+    [[8828,9581,49,9818,9974,9869,9991,10000,10000,10000,9999,9993,9904,8819,1231,6309], 134365, 16],
 
-    ([1,1], 3, -1),
+    [[5207,5594,477,6938,8010,7606,2356,6349,3970,751,5997,6114,9903,3859,6900,7722,2378,1996,8902,228,4461,90,7321,7893,4879,9987,1146,8177,1073,7254,5088,402,4266,6443,3084,1403,5357,2565,3470,3639,9468,8932,3119,5839,8008,2712,2735,825,4236,3703,2711,530,9630,1521,2174,5027,4833,3483,445,8300,3194,8784,279,3097,1491,9864,4992,6164,2043,5364,9192,9649,9944,7230,7224,585,3722,5628,4833,8379,3967,5649,2554,5828,4331,3547,7847,5433,3394,4968,9983,3540,9224,6216,9665,8070,31,3555,4198,2626,9553,9724,4503,1951,9980,3975,6025,8928,2952,911,3674,6620,3745,6548,4985,5206,5777,1908,6029,2322,2626,2188,5639], 565610, 113],
 
-    ([1,1,4,2,3], 5, 2),
-    ([5,6,7,8,9], 4, -1),
-    ([3,2,20,1,1,3], 10, 5)
+    [[1,1], 3, -1],
+
+    [[1,1,4,2,3], 5, 2],
+    [[5,6,7,8,9], 4, -1],
+    [[3,2,20,1,1,3], 10, 5]
 ]
 
 run_functional_tests(Solution().minOperations, tests)
