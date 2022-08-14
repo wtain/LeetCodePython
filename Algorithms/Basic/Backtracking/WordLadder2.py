6d7fd246673ec9816cb47a67dd111d27f55e5cfb
Ramiz <ramiz.zeynalov@gmail.com>
2022-08-14 02:28:20 +0200

    Daily python hard

--- Algorithms/Basic/Backtracking/WordLadder2.py
+++ Algorithms/Basic/Backtracking/WordLadder2.py
@@ -36,7 +36,7 @@ beginWord != endWord
 All the words in wordList are unique.
 """
 import collections
-from typing import List, Set
+from typing import List, Set, Dict
 
 from Common.ObjectTestingUtils import run_functional_tests
 from Common.Helpers.ResultComparators import compareSets
@@ -192,89 +192,91 @@ from Common.Helpers.ResultComparators import compareSets
 #                 neighbours.append(new_word)
 #         return neighbours
 
+
+# TLE
 # https://leetcode.com/problems/word-ladder-ii/solution/
 # Runtime: 48 ms, faster than 79.10% of Python3 online submissions for Word Ladder II.
 # Memory Usage: 14.5 MB, less than 87.51% of Python3 online submissions for Word Ladder II.
-class Solution:
-
-    def findNeighbours(self, word, wordList):
-        neighbours = []
-        m = len(word)
-        for i in range(m):
-            oldChar = word[i]
-            for c in [chr(ord('a')+i) for i in range(26)]:
-                new_word = word[:i] + c + word[i+1:]
-                if c == oldChar or new_word not in wordList:
-                    continue
-                neighbours.append(new_word)
-        return neighbours
-
-    def backtrack(self, source: str, destination: str):
-        if source == destination:
-            tempPath = self.currPath.copy()
-            self.shortestPaths.append(tempPath)
-        if source not in self.adjList:
-            return
-        adjs = self.adjList[source]
-        for adj in adjs:
-            self.currPath.append(adj)
-            self.backtrack(adj, destination)
-            self.currPath.pop()
-
-    def addEdge(self, w1: str, w2: str, direction: int):
-        if direction == 1:
-            self.adjList[w1].append(w2)
-        else:
-            self.adjList[w2].append(w1)
-
-    def bfs(self, beginWord: str, endWord: str, wordList: Set[str]) -> bool:
-        if endWord not in wordList:
-            return False
-
-        if beginWord in wordList:
-            wordList.remove(beginWord)
-
-        q1 = [beginWord]
-        q2 = [endWord]
-
-        found = False
-        direction = 1
-        while q1:
-            visited = set()
-            if len(q1) > len(q2):
-                q1, q2 = q2, q1
-                direction ^= 1
-            for curr_word in q1:
-                neighbours = self.findNeighbours(curr_word, wordList)
-                for word in neighbours:
-                    if word in q2:
-                        found = True
-                        self.addEdge(curr_word, word, direction)
-                    elif not found and word in wordList and word not in q1:
-                        visited.add(word)
-                        self.addEdge(curr_word, word, direction)
-            for curr_word in q1:
-                if curr_word in wordList:
-                    wordList.remove(curr_word)
-            if found:
-                break
-
-            q1 = visited
-        return found
-
-    def findLadders(self, beginWord: str, endWord: str, wordList: List[str]) -> List[List[str]]:
-        self.currPath = []
-        self.adjList = collections.defaultdict(list)
-        self.shortestPaths = []
-
-        words = set(wordList)
-        found = self.bfs(beginWord, endWord, words)
-        if not found:
-            return self.shortestPaths
-        self.currPath.append(beginWord)
-        self.backtrack(beginWord, endWord)
-
-        return self.shortestPaths
+# class Solution:
+#
+#     def findNeighbours(self, word, wordList):
+#         neighbours = []
+#         m = len(word)
+#         for i in range(m):
+#             oldChar = word[i]
+#             for c in [chr(ord('a')+i) for i in range(26)]:
+#                 new_word = word[:i] + c + word[i+1:]
+#                 if c == oldChar or new_word not in wordList:
+#                     continue
+#                 neighbours.append(new_word)
+#         return neighbours
+#
+#     def backtrack(self, source: str, destination: str):
+#         if source == destination:
+#             tempPath = self.currPath.copy()
+#             self.shortestPaths.append(tempPath)
+#         if source not in self.adjList:
+#             return
+#         adjs = self.adjList[source]
+#         for adj in adjs:
+#             self.currPath.append(adj)
+#             self.backtrack(adj, destination)
+#             self.currPath.pop()
+#
+#     def addEdge(self, w1: str, w2: str, direction: int):
+#         if direction == 1:
+#             self.adjList[w1].append(w2)
+#         else:
+#             self.adjList[w2].append(w1)
+#
+#     def bfs(self, beginWord: str, endWord: str, wordList: Set[str]) -> bool:
+#         if endWord not in wordList:
+#             return False
+#
+#         if beginWord in wordList:
+#             wordList.remove(beginWord)
+#
+#         q1 = [beginWord]
+#         q2 = [endWord]
+#
+#         found = False
+#         direction = 1
+#         while q1:
+#             visited = set()
+#             if len(q1) > len(q2):
+#                 q1, q2 = q2, q1
+#                 direction ^= 1
+#             for curr_word in q1:
+#                 neighbours = self.findNeighbours(curr_word, wordList)
+#                 for word in neighbours:
+#                     if word in q2:
+#                         found = True
+#                         self.addEdge(curr_word, word, direction)
+#                     elif not found and word in wordList and word not in q1:
+#                         visited.add(word)
+#                         self.addEdge(curr_word, word, direction)
+#             for curr_word in q1:
+#                 if curr_word in wordList:
+#                     wordList.remove(curr_word)
+#             if found:
+#                 break
+#
+#             q1 = visited
+#         return found
+#
+#     def findLadders(self, beginWord: str, endWord: str, wordList: List[str]) -> List[List[str]]:
+#         self.currPath = []
+#         self.adjList = collections.defaultdict(list)
+#         self.shortestPaths = []
+#
+#         words = set(wordList)
+#         found = self.bfs(beginWord, endWord, words)
+#         if not found:
+#             return self.shortestPaths
+#         self.currPath.append(beginWord)
+#         self.backtrack(beginWord, endWord)
+#
+#         return self.shortestPaths
 
 
 # UNFINISHED
@@ -322,6 +324,78 @@ class Solution:
 #         return result
 
 
+# Runtime: 108 ms, faster than 44.34% of Python3 online submissions for Word Ladder II.
+# Memory Usage: 14.3 MB, less than 68.67% of Python3 online submissions for Word Ladder II.
+# https://leetcode.com/problems/word-ladder-ii/discuss/2411758/Java-Solution-DFS%2BBFS
+class Solution:
+
+    def findLadders(self, beginWord: str, endWord: str, wordList: List[str]) -> List[List[str]]:
+        master = []
+        dictionary = set(wordList)
+        if len(beginWord) != len(endWord) or endWord not in dictionary:
+            return master
+        if beginWord == endWord and endWord in dictionary:
+            return [[beginWord]]
+
+        visitedAtLevel = {}
+        graph = collections.defaultdict(list)
+        self.bfs(beginWord, endWord, dictionary, visitedAtLevel, graph)
+        self.currPath = []
+        self.visited = set()
+        self.dfs(graph, master, beginWord, endWord)
+        return master
+
+    def bfs(self, start, end, dictionary, visitedAtLevel: Dict[str, int], graph):
+        q = [start]
+        visitedAtLevel[start] = 0
+        while q:
+            currWord = q[0]
+            q = q[1:]
+            arr = [c for c in currWord]
+            for i in range(len(arr)):
+                currChar = arr[i]
+                for j in "abcdefghijklmnopqrstuvwxyz":
+                    if arr[i] == j:
+                        continue
+                    arr[i] = j
+                    temp = "".join(arr)
+                    if temp in dictionary:
+                        if temp not in visitedAtLevel:
+                            v = -1
+                            if currWord in visitedAtLevel:
+                                v = visitedAtLevel[currWord]
+                            visitedAtLevel[temp] = v+1
+                            if currWord not in graph:
+                                graph[currWord] = []
+                            graph[currWord].append(temp)
+                            q.append(temp)
+                        elif visitedAtLevel[temp] == visitedAtLevel[currWord] + 1:
+                            if currWord not in graph:
+                                graph[currWord] = []
+                            graph[currWord].append(temp)
+                arr[i] = currChar
+
+    def dfs(self, graph, master, start, end):
+        if start in self.visited:
+            return
+        if start == end:
+            self.currPath.append(start)
+            master.append(self.currPath)
+            self.currPath = self.currPath[:len(self.currPath)-1]
+            self.visited = set()
+            return
+
+        if not graph[start]:
+            return
+
+        self.visited.add(start)
+        for curr in graph[start]:
+            self.currPath.append(start)
+            self.dfs(graph, master, curr, end)
+            if self.currPath:
+                self.currPath = self.currPath[:len(self.currPath) - 1]
+
+
 tests = [
     [
         "set",
@@ -356,6 +430,11 @@ tests = [
         "hit", "cog",
         ["hot","dot","dog","lot","log"],
         []
+    ],
+    [
+        "aaaaa", "uuuuu",
+        ["aaaaa","waaaa","wbaaa","xaaaa","xbaaa","bbaaa","bbwaa","bbwba","bbxaa","bbxba","bbbba","wbbba","wbbbb","xbbba","xbbbb","cbbbb","cwbbb","cwcbb","cxbbb","cxcbb","cccbb","cccwb","cccwc","cccxb","cccxc","ccccc","wcccc","wdccc","xcccc","xdccc","ddccc","ddwcc","ddwdc","ddxcc","ddxdc","ddddc","wdddc","wdddd","xdddc","xdddd","edddd","ewddd","ewedd","exddd","exedd","eeedd","eeewd","eeewe","eeexd","eeexe","eeeee","weeee","wfeee","xeeee","xfeee","ffeee","ffwee","ffwfe","ffxee","ffxfe","ffffe","wfffe","wffff","xfffe","xffff","gffff","gwfff","gwgff","gxfff","gxgff","gggff","gggwf","gggwg","gggxf","gggxg","ggggg","wgggg","whggg","xgggg","xhggg","hhggg","hhwgg","hhwhg","hhxgg","hhxhg","hhhhg","whhhg","whhhh","xhhhg","xhhhh","ihhhh","iwhhh","iwihh","ixhhh","ixihh","iiihh","iiiwh","iiiwi","iiixh","iiixi","iiiii","wiiii","wjiii","xiiii","xjiii","jjiii","jjwii","jjwji","jjxii","jjxji","jjjji","wjjji","wjjjj","xjjji","xjjjj","kjjjj","kwjjj","kwkjj","kxjjj","kxkjj","kkkjj","kkkwj","kkkwk","kkkxj","kkkxk","kkkkk","wkkkk","wlkkk","xkkkk","xlkkk","llkkk","llwkk","llwlk","llxkk","llxlk","llllk","wlllk","wllll","xlllk","xllll","mllll","mwlll","mwmll","mxlll","mxmll","mmmll","mmmwl","mmmwm","mmmxl","mmmxm","mmmmm","wmmmm","wnmmm","xmmmm","xnmmm","nnmmm","nnwmm","nnwnm","nnxmm","nnxnm","nnnnm","wnnnm","wnnnn","xnnnm","xnnnn","onnnn","ownnn","owonn","oxnnn","oxonn","ooonn","ooown","ooowo","oooxn","oooxo","ooooo","woooo","wpooo","xoooo","xpooo","ppooo","ppwoo","ppwpo","ppxoo","ppxpo","ppppo","wpppo","wpppp","xpppo","xpppp","qpppp","qwppp","qwqpp","qxppp","qxqpp","qqqpp","qqqwp","qqqwq","qqqxp","qqqxq","qqqqq","wqqqq","wrqqq","xqqqq","xrqqq","rrqqq","rrwqq","rrwrq","rrxqq","rrxrq","rrrrq","wrrrq","wrrrr","xrrrq","xrrrr","srrrr","swrrr","swsrr","sxrrr","sxsrr","sssrr","ssswr","sssws","sssxr","sssxs","sssss","wssss","wtsss","xssss","xtsss","ttsss","ttwss","ttwts","ttxss","ttxts","tttts","wttts","wtttt","xttts","xtttt","utttt","uwttt","uwutt","uxttt","uxutt","uuutt","uuuwt","uuuwu","uuuxt","uuuxu","uuuuu","zzzzz","zzzzy","zzzyy","zzyyy","zzyyx","zzyxx","zzxxx","zzxxw","zzxww","zzwww","zzwwv","zzwvv","zzvvv","zzvvu","zzvuu","zzuuu","zzuut","zzutt","zzttt","zztts","zztss","zzsss","zzssr","zzsrr","zzrrr","zzrrq","zzrqq","zzqqq","zzqqp","zzqpp","zzppp","zzppo","zzpoo","zzooo","zzoon","zzonn","zznnn","zznnm","zznmm","zzmmm","zzmml","zzmll","zzlll","zzllk","zzlkk","zzkkk","zzkkj","zzkjj","zzjjj","zzjji","zzjii","zziii","zziih","zzihh","zzhhh","zzhhg","zzhgg","zzggg","zzggf","zzgff","zzfff","zzffe","zzfee","zzeee","zzeed","zzedd","zzddd","zzddc","zzdcc","zzccc","zzccz","azccz","aaccz","aaacz","aaaaz","uuuzu","uuzzu","uzzzu","zzzzu"],
+        [["aaaaa","aaaaz","aaacz","aaccz","azccz","zzccz","zzccc","zzdcc","zzddc","zzddd","zzedd","zzeed","zzeee","zzfee","zzffe","zzfff","zzgff","zzggf","zzggg","zzhgg","zzhhg","zzhhh","zzihh","zziih","zziii","zzjii","zzjji","zzjjj","zzkjj","zzkkj","zzkkk","zzlkk","zzllk","zzlll","zzmll","zzmml","zzmmm","zznmm","zznnm","zznnn","zzonn","zzoon","zzooo","zzpoo","zzppo","zzppp","zzqpp","zzqqp","zzqqq","zzrqq","zzrrq","zzrrr","zzsrr","zzssr","zzsss","zztss","zztts","zzttt","zzutt","zzuut","zzuuu","zzvuu","zzvvu","zzvvv","zzwvv","zzwwv","zzwww","zzxww","zzxxw","zzxxx","zzyxx","zzyyx","zzyyy","zzzyy","zzzzy","zzzzu","uzzzu","uuzzu","uuuzu","uuuuu"]]
     ]
 ]
 
