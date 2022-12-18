824bfad1a1c54b362872c0cf4335dc34c739fba0
Ramiz Zeynalov <ramiz.zeynalov@gmail.com>
2022-12-18 19:54:25 +0100

    Medium - better approach

--- DataStructures/Advanced/Graph/DepthFirstSearch/BulbSwitcher2.py
+++ DataStructures/Advanced/Graph/DepthFirstSearch/BulbSwitcher2.py
@@ -194,31 +194,48 @@ from Common.ObjectTestingUtils import run_functional_tests
 # 14 MB
 # Beats
 # 42.98%
+# class Solution:
+#     def flipLights(self, n: int, presses: int) -> int:
+#
+#         all_ones, evens, odds, three = 0, 0, 0, 0
+#         for i in range(n):
+#             all_ones = (all_ones << 1) + 1
+#             evens = (evens << 1) + (1 if (i+1) % 2 == 0 else 0)
+#             odds = (odds << 1) + (1 if (i+1) % 2 == 1 else 0)
+#             three = (three << 1) + (1 if (i+1) % 3 == 1 else 0)
+#
+#         state = all_ones
+#
+#         level = {state}
+#         for i in range(presses):
+#             next_level = set()
+#             for state in level:
+#                 states = [state ^ all_ones,
+#                           state ^ evens,
+#                           state ^ odds,
+#                           state ^ three]
+#                 for next_state in states:
+#                     next_level.add(next_state)
+#             level = next_level
+#
+#         return len(level)
+
+
+# Runtime
+# 35 ms
+# Beats
+# 89.47%
+# Memory
+# 13.8 MB
+# Beats
+# 74.56%
 class Solution:
     def flipLights(self, n: int, presses: int) -> int:
-
-        all_ones, evens, odds, three = 0, 0, 0, 0
-        for i in range(n):
-            all_ones = (all_ones << 1) + 1
-            evens = (evens << 1) + (1 if (i+1) % 2 == 0 else 0)
-            odds = (odds << 1) + (1 if (i+1) % 2 == 1 else 0)
-            three = (three << 1) + (1 if (i+1) % 3 == 1 else 0)
-
-        state = all_ones
-
-        level = {state}
-        for i in range(presses):
-            next_level = set()
-            for state in level:
-                states = [state ^ all_ones,
-                          state ^ evens,
-                          state ^ odds,
-                          state ^ three]
-                for next_state in states:
-                    next_level.add(next_state)
-            level = next_level
-
-        return len(level)
+        if n == 1:
+            return 1 if presses == 0 else 2
+        if n == 2:
+            return 1 if presses == 0 else (3 if presses == 1 else 4)
+        return 1 if presses == 0 else (4 if presses == 1 else (7 if presses == 2 else 8))
 
 
 tests = [
