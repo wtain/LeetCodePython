20d27a1dd03d9fe66fe55dcb843af5f5a4e41644
Ramiz Zeynalov <ramiz.zeynalov@gmail.com>
2023-02-14 00:02:34 +0100

    Medium

---
+++ Algorithms/Advanced/DynamicProgramming/Strings/VowelsOfAllSubstrings.py
@@ -0,0 +1,71 @@
+"""
+https://leetcode.com/problems/vowels-of-all-substrings/
+
+Given a string word, return the sum of the number of vowels ('a', 'e', 'i', 'o', and 'u') in every substring of word.
+
+A substring is a contiguous (non-empty) sequence of characters within a string.
+
+Note: Due to the large constraints, the answer may not fit in a signed 32-bit integer. Please be careful during the calculations.
+
+
+
+Example 1:
+
+Input: word = "aba"
+Output: 6
+Explanation:
+All possible substrings are: "a", "ab", "aba", "b", "ba", and "a".
+- "b" has 0 vowels in it
+- "a", "ab", "ba", and "a" have 1 vowel each
+- "aba" has 2 vowels in it
+Hence, the total sum of vowels = 0 + 1 + 1 + 1 + 1 + 2 = 6.
+Example 2:
+
+Input: word = "abc"
+Output: 3
+Explanation:
+All possible substrings are: "a", "ab", "abc", "b", "bc", and "c".
+- "a", "ab", and "abc" have 1 vowel each
+- "b", "bc", and "c" have 0 vowels each
+Hence, the total sum of vowels = 1 + 1 + 1 + 0 + 0 + 0 = 3.
+Example 3:
+
+Input: word = "ltcd"
+Output: 0
+Explanation: There are no vowels in any substring of "ltcd".
+
+
+Constraints:
+
+1 <= word.length <= 105
+word consists of lowercase English letters.
+"""
+from Common.ObjectTestingUtils import run_functional_tests
+
+
+# Runtime
+# 113 ms
+# Beats
+# 94.35%
+# Memory
+# 14.8 MB
+# Beats
+# 80.24%
+class Solution:
+    def countVowels(self, word: str) -> int:
+        result = 0
+        current = 0
+        for i, c in enumerate(word):
+            if c in "aeiou":
+                current += (i+1)
+            result += current
+        return result
+
+
+tests = [
+    ["aba", 6],
+    ["abc", 3],
+    ["ltcd", 0],
+]
+
+run_functional_tests(Solution().countVowels, tests)
