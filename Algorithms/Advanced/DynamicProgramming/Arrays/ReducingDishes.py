5ffb84697b0b042a4831d0479a1c2ee68771103e
Ramiz Zeynalov <ramiz.zeynalov@gmail.com>
2023-03-29 11:50:25 +0200

    Daily hard

---
+++ Algorithms/Advanced/DynamicProgramming/Arrays/ReducingDishes.py
@@ -0,0 +1,69 @@
+"""
+https://leetcode.com/problems/reducing-dishes/description/
+
+A chef has collected data on the satisfaction level of his n dishes. Chef can cook any dish in 1 unit of time.
+
+Like-time coefficient of a dish is defined as the time taken to cook that dish including previous dishes multiplied by its satisfaction level i.e. time[i] * satisfaction[i].
+
+Return the maximum sum of like-time coefficient that the chef can obtain after dishes preparation.
+
+Dishes can be prepared in any order and the chef can discard some dishes to get this maximum value.
+
+
+
+Example 1:
+
+Input: satisfaction = [-1,-8,0,5,-9]
+Output: 14
+Explanation: After Removing the second and last dish, the maximum total like-time coefficient will be equal to (-1*1 + 0*2 + 5*3 = 14).
+Each dish is prepared in one unit of time.
+Example 2:
+
+Input: satisfaction = [4,3,2]
+Output: 20
+Explanation: Dishes can be prepared in any order, (2*1 + 3*2 + 4*3 = 20)
+Example 3:
+
+Input: satisfaction = [-1,-4,-5]
+Output: 0
+Explanation: People do not like the dishes. No dish is prepared.
+
+
+Constraints:
+
+n == satisfaction.length
+1 <= n <= 500
+-1000 <= satisfaction[i] <= 1000
+"""
+from typing import List
+
+from Common.ObjectTestingUtils import run_functional_tests
+
+
+# Runtime
+# 36 ms
+# Beats
+# 93.67%
+# Memory
+# 14 MB
+# Beats
+# 25%
+class Solution:
+    def maxSatisfaction(self, satisfaction: List[int]) -> int:
+        satisfaction.sort(reverse=True)
+        result, current, ps = 0, 0, 0
+        n = len(satisfaction)
+        for i in range(n):
+            current += ps + satisfaction[i]
+            ps += satisfaction[i]
+            result = max(result, current)
+        return result
+
+
+tests = [
+    [[-1,-8,0,5,-9], 14],
+    [[4,3,2], 20],
+    [[-1,-4,-5], 0],
+]
+
+run_functional_tests(Solution().maxSatisfaction, tests)
