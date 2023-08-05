a25b6993fba713ed2ed265f80a59828b9b7c2978
Ramiz Zeynalov <ramiz.zeynalov@gmail.com>
2023-08-05 15:58:42 +0200

    Daily medium

--- Algorithms/Advanced/DynamicProgramming/Trees/UniqueBinarySearchTrees2.py
+++ Algorithms/Advanced/DynamicProgramming/Trees/UniqueBinarySearchTrees2.py
@@ -41,50 +41,108 @@ from Common.TreeUtils import build_tree_list_from_lists
 
 # Runtime: 109 ms, faster than 5.04% of Python3 online submissions for Unique Binary Search Trees II.
 # Memory Usage: 18.5 MB, less than 5.15% of Python3 online submissions for Unique Binary Search Trees II.
+# class Solution:
+#     def generateTrees(self, n: int) -> List[Optional[TreeNode]]:
+#         if not n:
+#             return []
+#
+#         used = [False] * n
+#
+#         def cloneTree(node: TreeNode) -> TreeNode:
+#             if not node:
+#                 return None
+#             result = TreeNode(node.val)
+#             result.left = cloneTree(node.left)
+#             result.right = cloneTree(node.right)
+#             return result
+#
+#         @lru_cache(None)
+#         def impl(vmin: int, vmax: int) -> List[TreeNode]:
+#             if vmin > vmax:
+#                 return [None]
+#             result = []
+#             for i in range(n):
+#                 v = i+1
+#                 if used[i]:
+#                     continue
+#                 if not (vmin <= v <= vmax):
+#                     continue
+#
+#                 used[i] = True
+#                 node = TreeNode(v)
+#
+#                 rl = impl(vmin, v-1)
+#                 rr = impl(v+1, vmax)
+#                 for rli in rl:
+#                     node.left = rli
+#                     for rri in rr:
+#                         node.right = rri
+#                         result.append(cloneTree(node))
+#
+#                 used[i] = False
+#             if not result:
+#                 result.append(None)
+#             return result
+#
+#         return impl(1, n)
+
+
+# Runtime
+# Details
+# 52ms
+# Beats 97.94%of users with Python3
+# Memory
+# Details
+# 17.18mb
+# Beats 93.75%of users with Python3
+# https://leetcode.com/problems/unique-binary-search-trees-ii/editorial/
+# class Solution:
+#     def generateTrees(self, n: int) -> List[Optional[TreeNode]]:
+#         dp = [[[] for _ in range(n+1)] for _ in range(n+1)]
+#         for i in range(1, n+1):
+#             dp[i][i] = [TreeNode(i)]
+#         for nodes_cnt in range(2, n+1):
+#             for start in range(1, n - nodes_cnt+2):
+#                 end = start + nodes_cnt - 1
+#                 for i in range(start, end+1):
+#                     left_subtrees = dp[start][i-1] if i != start else [None]
+#                     right_subtrees = dp[i+1][end] if i != end else [None]
+#
+#                     for left in left_subtrees:
+#                         for right in right_subtrees:
+#                             root = TreeNode(i, left, right)
+#                             dp[start][end].append(root)
+#         return dp[1][n]
+
+# Runtime
+# Details
+# 63ms
+# Beats 81.38%of users with Python3
+# Memory
+# Details
+# 18.23mb
+# Beats 20.17%of users with Python3
+# https://leetcode.com/problems/unique-binary-search-trees-ii/editorial/
 class Solution:
     def generateTrees(self, n: int) -> List[Optional[TreeNode]]:
-        if not n:
-            return []
+        dp = [[] for _ in range(n+1)]
+        dp[0].append(None)
+        for nodes_cnt in range(1, n+1):
+            for i in range(1, nodes_cnt+1):
+                j = nodes_cnt - i
+                for left in dp[i-1]:
+                    for right in dp[j]:
+                        root = TreeNode(i, left, self.clone(right, i))
+                        dp[nodes_cnt].append(root)
+        return dp[n]
 
-        used = [False] * n
-
-        def cloneTree(node: TreeNode) -> TreeNode:
-            if not node:
-                return None
-            result = TreeNode(node.val)
-            result.left = cloneTree(node.left)
-            result.right = cloneTree(node.right)
-            return result
-
-        @lru_cache(None)
-        def impl(vmin: int, vmax: int) -> List[TreeNode]:
-            if vmin > vmax:
-                return [None]
-            result = []
-            for i in range(n):
-                v = i+1
-                if used[i]:
-                    continue
-                if not (vmin <= v <= vmax):
-                    continue
-
-                used[i] = True
-                node = TreeNode(v)
-
-                rl = impl(vmin, v-1)
-                rr = impl(v+1, vmax)
-                for rli in rl:
-                    node.left = rli
-                    for rri in rr:
-                        node.right = rri
-                        result.append(cloneTree(node))
-
-                used[i] = False
-            if not result:
-                result.append(None)
-            return result
-
-        return impl(1, n)
+    def clone(self, node, offset):
+        if not node:
+            return None
+        cloned_node = TreeNode(node.val + offset)
+        cloned_node.left = self.clone(node.left, offset)
+        cloned_node.right = self.clone(node.right, offset)
+        return cloned_node
 
 
 tests = [
