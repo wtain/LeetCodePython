55962cae9aae5757a40b6fe75f75afcb269e2f63
Ramiz Zeynalov <ramiz.zeynalov@gmail.com>
2024-05-08 04:14:37 +0200

    Daily easy - another approach

--- DataStructures/Basic/Heaps/RelativeRanks.py
+++ DataStructures/Basic/Heaps/RelativeRanks.py
@@ -51,23 +51,57 @@ from Common.ObjectTestingUtils import run_functional_tests
 # Beats
 # 50.24%
 # of users with Python3
+# class Solution:
+#     def findRelativeRanks(self, score: List[int]) -> List[str]:
+#         n = len(score)
+#         entries = [(score[i], i) for i in range(n)]
+#         entries.sort(reverse=True)
+#         result = [0] * n
+#         for j in range(n):
+#             i = entries[j][1]
+#             if j == 0:
+#                 result[i] = "Gold Medal"
+#             elif j == 1:
+#                 result[i] = "Silver Medal"
+#             elif j == 2:
+#                 result[i] = "Bronze Medal"
+#             else:
+#                 result[i] = str(j+1)
+#         return result
+
+
+# Runtime
+# 67
+# ms
+# Beats
+# 51.66%
+# of users with Python3
+# Memory
+# 17.68
+# MB
+# Beats
+# 71.46%
+# of users with Python3
+# https://leetcode.com/problems/relative-ranks/editorial/?envType=daily-question&envId=2024-05-08
 class Solution:
     def findRelativeRanks(self, score: List[int]) -> List[str]:
         n = len(score)
-        entries = [(score[i], i) for i in range(n)]
-        entries.sort(reverse=True)
-        result = [0] * n
-        for j in range(n):
-            i = entries[j][1]
-            if j == 0:
-                result[i] = "Gold Medal"
-            elif j == 1:
-                result[i] = "Silver Medal"
-            elif j == 2:
-                result[i] = "Bronze Medal"
-            else:
-                result[i] = str(j+1)
-        return result
+        m = max(score)
+        score_to_index = [0] * (m+1)
+        for i in range(n):
+            score_to_index[score[i]] = i+1
+        MEDALS = ["Gold Medal", "Silver Medal", "Bronze Medal"]
+        rank = [None] * n
+        place = 1
+        for i in range(m, -1, -1):
+            if score_to_index[i] != 0:
+                original_index = score_to_index[i] - 1
+                if place < 4:
+                    rank[original_index] = MEDALS[place-1]
+                else:
+                    rank[original_index] = str(place)
+                place += 1
+        return rank
 
 
 tests = [
